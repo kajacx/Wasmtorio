a9fdd4dc9dc368baad1e2fdeeb6608e39a4c7454
kajacx <kajacx@gmail.com>
2025-05-22 15:54:31 +0200

    Compile rust plugin to lua with Wasynth

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 set -e
 
 cd plugins/rust
-cargo build --target=wasm32-unknown-unknown
+cargo build --target=wasm32-unknown-unknown --release
+
+cargo run --manifest-path /e/Programming/Rust/Wasynth/codegen/luajit/Cargo.toml target/wasm32-unknown-unknown/release/wasmtorio_rust_plugin.wasm > rust-control.lua
+
+
+# wasm2lua abi.optimized.wat.wasm rust-control.lua
+# wasm2lua target/wasm32-unknown-unknown/release/wasmtorio_rust_plugin.wasm rust-control.lua
 cd ../..
 
 echo "All done."
--- plugins/rust/src/lib.rs
+++ plugins/rust/src/lib.rs
@@ -2,3 +2,18 @@
 pub fn add_five_i32(number: i32) -> i32 {
     number.wrapping_add(5)
 }
+
+// #[no_mangle]
+// pub fn add_five_i64(number: i64) -> i64 {
+//     number.wrapping_add(5)
+// }
+
+// #[no_mangle]
+// pub fn add_five_f32(number: f32) -> f32 {
+//     number + 5.0
+// }
+
+// #[no_mangle]
+// pub fn add_five_f64(number: f64) -> f64 {
+//     number + 5.0
+// }
